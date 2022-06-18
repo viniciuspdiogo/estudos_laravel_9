b6e9af354969407db7ca9c0d9edb8a9678014103
viniciuspdiogo <viniciusp.diogo@hotmail.com>
2022-06-18 12:56:50 -0300

    Add Comments

---
+++ app/Models/Comment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillabkle= [
+        'body',
+        'visible',
+    ];
+
+    protected $casts = [
+        'visible' => 'boolean'
+    ];
+}
